5f238d1fd22ff8526964bf12bd4fb346a0fbdb3b
Sakurayia <709929545@qq.com>
2023-10-17 09:09:35 +0200

    change dataset.py

--- dataset.py
+++ dataset.py
@@ -1,16 +1,23 @@
 from torch.utils.data import Dataset
 import pandas as pd
+import numpy as np
 
 class ECGDataset(Dataset):
-    def __init__(self, path):
+    def __init__(self, path, transform=None, target_transform=None):
         dataframe = pd.read_csv(path, header=None)
-        self.data = dataframe.iloc[:, 0:-1]
-        self.labels = dataframe.iloc[:, -1:]
+        self.data = np.array(dataframe.iloc[:, 0:-1])
+        self.labels = np.array(dataframe.iloc[:, -1:]).astype('int')
+        self.transform = transform
+        self.target_transform = target_transform
 
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self, index):
-        x = self.data[index]
-        y = self.labels[index]
-        return x, y
+        data = self.data[index]
+        label = self.labels[index]
+        if self.transform:
+            data = self.transform(data)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return data, label
